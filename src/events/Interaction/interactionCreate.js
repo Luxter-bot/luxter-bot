489806794a6476bc9b047299ba6ef9adde66231b
xTheSebaSx <61850374+xTheSebaSx-afk@users.noreply.github.com>
2022-06-28 10:02:59 -0500

    bot permissions

--- src/events/Interaction/interactionCreate.js
+++ src/events/Interaction/interactionCreate.js
@@ -2,6 +2,7 @@ import {Formatters, InteractionCollector} from "discord.js"
 import {Event} from "mitim"
 import Discord from "discord.js"
 import PermissionConfig from "../../schemas/permissions.js"
+import Welcome from "../../schemas/welcome.js"
 
 export default new Event({
     name: "interactionCreate",
@@ -14,6 +15,16 @@ export default new Event({
             const command = bot.ApplicationCommand.get(interaction.commandName)
 
             if (!command) return
+
+            if (command.permissions?.bot) {
+                if (!interaction.member.permissions.has(command.permissions.bot))
+                    return interaction.reply({
+                        content: `❌ | I don't have the required permissions to use this command\n${Formatters.codeBlock(
+                            command.permissions.bot.join(", ")
+                        )}`,
+                        ephemeral: true
+                    })
+            }
 
             if (permissions === "default") {
                 if (command.permissions) {
@@ -178,6 +189,107 @@ export default new Event({
                     })
                 }
             }
+
+            if (interaction.customId === "modal-welcome") {
+                const title = interaction.fields.getTextInputValue("title-welcome")
+                const description = interaction.fields.getTextInputValue("description-welcome")
+                const color = interaction.fields.getTextInputValue("color-welcome")
+                const thumbnail = interaction.fields.getTextInputValue("thumbnail-welcome") || null
+                const image = interaction.fields.getTextInputValue("image-welcome") || null
+
+                const data = await Welcome.findOne({_id: interaction.guild.id})
+
+                if (data) {
+                    data.message.embed.title = title
+                    data.message.embed.description = description
+                    data.message.embed.color = color
+                    data.message.embed.thumbnail = thumbnail
+                    data.message.embed.image = image
+
+                    await data.save()
+
+                    interaction.reply({
+                        content: "✅ | Welcome message updated"
+                    })
+                } else {
+                    if (isNaN(color)) {
+                        interaction.reply({
+                            content: "❌ | The color must be a number"
+                        })
+                    }
+
+                    const thumbnailURL = new URL(thumbnail)
+
+                    if (!["http:", "https:"].includes(thumbnailURL.protocol))
+                        return interaction.reply({
+                            content: "❌ | The thumbnail must be a URL"
+                        })
+
+                    const imageURL = new URL(image)
+
+                    if (!["http:", "https:"].includes(imageURL.protocol))
+                        return interaction.reply({
+                            content: "❌ | The image must be a URL"
+                        })
+
+                    const embed = new Discord.MessageEmbed()
+                        .setTitle(title)
+                        .setColor(InteractionCollector)
+                        .setDescription(description)
+                        .setThumbnail(thumbnailURL.href)
+                        .setImage(imageURL.href)
+
+                    interaction.reply({
+                        content: "✅ | Welcome message created"
+                    })
+
+                    interaction.channel.send({embeds: [embed]})
+
+                    new Welcome({
+                        _id: interaction.guild.id,
+                        message: {
+                            embed: {
+                                title: title,
+                                description: description,
+                                color: color,
+                                thumbnail: thumbnailURL.href,
+                                image: imageURL.href
+                            }
+                        }
+                    }).save()
+                }
+            }
+
+            if (interaction.customId === "modal-welcome-text") {
+                const content = interaction.fields.getTextInputValue("text-welcome")
+
+                const data = await Welcome.findOne({_id: interaction.guild.id})
+
+                if (data) {
+                    data.message.text = content
+
+                    await data.save()
+
+                    interaction.reply({
+                        content: "✅ | Welcome message updated"
+                    })
+                } else {
+                    interaction.reply({
+                        content: "✅ | Welcome message created"
+                    })
+
+                    interaction.channel.send({
+                        content: `${content}`
+                    })
+
+                    new Welcome({
+                        _id: interaction.guild.id,
+                        message: {
+                            text: content
+                        }
+                    }).save()
+                }
+            }
         }
 
         if (interaction.isAutocomplete()) {
@@ -194,6 +306,39 @@ export default new Event({
                 })
 
                 interaction.respond(commandArray)
+            }
+        }
+
+        if (interaction.isSelectMenu()) {
+            if (interaction.customId === "reaction-roles") {
+                await interaction.deferReply({ephemeral: true})
+
+                const roleId = interaction.values[0]
+                const role = interaction.guild.roles.cache.get(roleId)
+
+                const hasRole = interaction.member.roles.cache.has(roleId)
+
+                if (hasRole) {
+                    await interaction.member.roles.remove(roleId).catch((e) => {
+                        return interaction.editReply({
+                            content: `❌ | An unexpected error has occurred while removing the role: ${Formatters.codeBlock("prolog", e.message)}`
+                        })
+                    })
+
+                    interaction.editReply({
+                        content: `✅ | The role ${role} has been removed from you`
+                    })
+                } else {
+                    await interaction.member.roles.add(roleId).catch((e) => {
+                        return interaction.editReply({
+                            content: `❌ | An unexpected error has occurred while adding the role: ${Formatters.codeBlock("prolog", e.message)}`
+                        })
+                    })
+
+                    interaction.editReply({
+                        content: `✅ | The role ${role} has been added from you`
+                    })
+                }
             }
         }
     }
